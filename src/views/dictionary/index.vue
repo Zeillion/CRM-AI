1352103aac371145955dc68dbb9988a21bfadd20
leena <LINA898@crb.cn>
2020-12-31 17:52:50 +0800

    【feat】数据字典 模块UI开发

--- src/views/dictionary/index.vue
+++ src/views/dictionary/index.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="wrapper">
+    <div class="bold big_title">数据字典</div>
+    <div class="red_text">
+      *添加字段后请点击生效按钮；生效后增加的字段值无法修改，请谨慎填写；
+    </div>
     <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
       <el-tab-pane label="字段编辑" name="first">
         <el-table
           :data="tableData"
-          style="width: 100%"
+          style="width: 600px"
           :row-class-name="tableRowClassName"
           border
+          :header-cell-style="{
+            background: '#EFF2F7',
+            color: '#444',
+            'font-size': '14px',
+            'font-weight': 'bold',
+          }"
+          :cell-style="cellStyle"
         >
           <el-table-column prop="name" label="字段名"> </el-table-column>
           <el-table-column prop="data" label="字段值">
             <template slot-scope="scope">
-              <div v-for="(item,index) in scope.row.data" :key="index">{{item}} 
-                  <span v-show="scope.$index == 1 && index > 1">
-                       <el-button type="text" >修改</el-button>
-                  </span>
+              <div v-for="(item, index) in scope.row.data" :key="index">
+                {{ item }}
+                <span v-show="scope.$index == 1 && index > 1">
+                  <el-button type="text">修改</el-button>
+                </span>
               </div>
               <el-button type="text" v-if="scope.$index == 0">增加值</el-button>
             </template>
@@ -38,6 +50,10 @@ export default {
   props: {},
   data() {
     return {
+      cellStyle: {
+        "font-size": "14px",
+        color: "#1F2D3D",
+      },
       activeName: "first",
       tableData: [
         {
@@ -51,7 +67,7 @@ export default {
         },
         {
           name: "本竞品",
-          data: ["本品", "竟品","其他1"],
+          data: ["本品", "竟品", "其他1"],
         },
       ],
     };
@@ -63,4 +79,13 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.big_title {
+  margin-bottom: 20px;
+}
+.red_text {
+  margin-bottom: 20px;
+}
+.wrapper {
+  min-height: calc(100vh-60);
+}
 </style>
--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -26,6 +26,9 @@
               >
             </el-form-item>
           </div>
+          <div>
+            <el-button size="small" type="primary">导出</el-button>
+          </div>
         </div>
         <!-- 下拉选项组 -->
         <div class="flex_container">
@@ -66,49 +69,66 @@
     </div>
 
     <!-- table -->
-    <el-table
-      :data="tableData"
-      stripe
-      style="width: 100%"
-      :header-cell-style="{
-        background: '#EBEDF3',
-        color: '#444',
-        'font-size': '14px',
-      }"
-      :cell-style="cellStyle"
-    >
-      <el-table-column prop="img" label="24位图" fixed="left">
-      </el-table-column>
-      <el-table-column
-        prop="product"
-        label="商品全称"
-        fixed="left"
-        width="120,"
-      >
-      </el-table-column>
-      <el-table-column prop="barCode" label="条形码"> </el-table-column>
-      <el-table-column prop="packageCode" label="外观序号"> </el-table-column>
-      <el-table-column prop="type" label="商品类型"> </el-table-column>
-      <el-table-column prop="isSelf" label="本竞品"> </el-table-column>
-      <el-table-column prop="brand" label="商品品牌"> </el-table-column>
-      <el-table-column prop="packageType" label="包装形式"> </el-table-column>
-      <el-table-column prop="status" label="印射状态"> </el-table-column>
-      <el-table-column prop="time" label="新增时间"> </el-table-column>
 
-      <el-table-column label="操作" width="220">
-        <template slot-scope="{ row }">
-          <el-button size="small" type="text">停用</el-button>
-          <el-button size="small" type="text">映射</el-button>
-          <el-button size="small" type="text">修改</el-button>
-          <el-button size="small" type="text">
-            <span style="color:#034193">详情</span>
-          </el-button>
-          <el-button size="small" type="text">
-            <span style="color:#FE934F">操作记录</span>
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="approval_tab">
+      <el-tabs type="card">
+        <el-tab-pane label="全部SKU（1400）">
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%"
+            :header-cell-style="{
+              background: '#EBEDF3',
+              color: '#444',
+              'font-size': '14px',
+            }"
+            :cell-style="cellStyle"
+            ƒ
+          >
+            <el-table-column prop="img" label="24位图" fixed="left">
+            </el-table-column>
+            <el-table-column
+              prop="product"
+              label="商品全称"
+              fixed="left"
+              width="120,"
+            >
+            </el-table-column>
+            <el-table-column prop="barCode" label="条形码"> </el-table-column>
+            <el-table-column prop="packageCode" label="外观序号">
+            </el-table-column>
+            <el-table-column prop="type" label="商品类型"> </el-table-column>
+            <el-table-column prop="isSelf" label="本竞品"> </el-table-column>
+            <el-table-column prop="brand" label="商品品牌"> </el-table-column>
+            <el-table-column prop="packageType" label="包装形式">
+            </el-table-column>
+            <el-table-column prop="status" label="印射状态"> </el-table-column>
+            <el-table-column prop="time" label="新增时间"> </el-table-column>
+
+            <el-table-column label="操作" width="220">
+              <template slot-scope="{ row }">
+                <el-button
+                  size="small"
+                  type="text"
+                  @click="handleStop(row.stop)"
+                >
+                  <span v-if="row.stop == 0">停用</span>
+                  <span v-else>启用</span>
+                </el-button>
+                <el-button size="small" type="text">映射</el-button>
+                <el-button size="small" type="text">修改</el-button>
+                <el-button size="small" type="text">
+                  <span style="color: #034193">详情</span>
+                </el-button>
+                <el-button size="small" type="text">
+                  <span style="color: #fe934f">操作记录</span>
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
@@ -148,8 +168,9 @@ export default {
           reporter: "张三",
           phone: 18082823838,
           time: "2020-12-12 19:00:00",
+          stop: 0,
         },
-          {
+        {
           img: "",
           product: "雪花-脸谱300ml",
           barCode: 692319381,
@@ -165,29 +186,30 @@ export default {
           reporter: "张三",
           phone: 18082823838,
           time: "2020-12-12 19:00:00",
-        },
-          {
-          img: "",
-          product: "雪花-脸谱300ml",
-          barCode: 692319381,
-          packageCode: "11",
-          type: "POSM",
-          isSelf: "本品",
-          brand: "雪花",
-          AIID: "122331",
-          status: "CRM",
-          packageType: "瓶装",
-          time: "2020-09-22",
-          name: "脸谱300ml",
-          reporter: "张三",
-          phone: 18082823838,
-          time: "2020-12-12 19:00:00",
+          stop: 1,
         },
       ],
     };
   },
   methods: {
     onSubmit() {},
+    handleStop(data) {
+      let res = !data ? "停用" : "启用";
+      this.$confirm(
+        `是否${res}该商品，操作该将印象以后的数据，对之前的数据不产生影响。`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).then(() => {
+        this.$message({
+          type: "success",
+          message: "操作成功!",
+        });
+      });
+    },
   },
 };
 </script>
