ba840b2d563c3846a310d27c27d0ccde5f5ad5d2
leena <LINA898@crb.cn>
2021-01-07 16:50:16 +0800

    【feat】产品组合设置-首页

--- src/views/assemble/components/ProductAssemble.vue
+++ src/views/assemble/components/ProductAssemble.vue
@@ -22,29 +22,191 @@
         >
       </div>
     </div>
+
+    <div class="flex_container content">
+      <!-- 左侧组合树 -->
+      <div class="assemble_tree">
+        <el-tree
+          :data="data"
+          default-expand-all
+          :current-node-key="1"
+          node-key="id"
+          ref="tree"
+          highlight-current
+          :props="defaultProps"
+          @node-click="handleNodeClick"
+        >
+        </el-tree>
+      </div>
+      <!-- 右侧表格 -->
+      <component :is="componentName"></component>
+      <div class="flex">
+        <el-table
+          :data="tableData"
+          stripe
+          style="width: 100%"
+          :header-cell-style="{
+            background: '#EFF2F7',
+            color: '#444',
+          }"
+          :cell-style="cellStyle"
+          size="small"
+        >
+          <el-table-column label="组合名称">
+            <template slot-scope="{ row }">
+              <span>{{ row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="层级数" prop="layer"> </el-table-column>
+          <el-table-column label="包含商品数" prop="num"> </el-table-column>
+          <el-table-column label="创建日期" prop="createTime" width="120">
+          </el-table-column>
+          <el-table-column label="修改日期" prop="modifyTime" width="120">
+          </el-table-column>
+          <el-table-column label="备注信息" prop="remark"> </el-table-column>
+          <el-table-column label="操作" width="180">
+            <template slot-scope="{ row }">
+              <el-button size="small" type="text" @click="showDetail">
+                <span style="color: #ff737e">删除</span>
+              </el-button>
+              <el-button size="small" type="text" @click="showDetail">
+                <span style="color: #1d8ce0">修改</span>
+              </el-button>
+              <el-button size="small" type="text" @click="showDetail">
+                <span style="color: #034193">查看效果</span>
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+    <add-assemble ref="add"></add-assemble>
   </div>
 </template>
 
 <script>
+import AddAssemble from "../dialogs/addAssemble.vue";
 export default {
   name: "ProductAssemble",
-  components: {},
+  components: { AddAssemble },
   props: {},
   data() {
     return {
       name: "",
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+      tableData: [
+        {
+          name: "0-5元",
+          layer: 2,
+          num: 20,
+          createTime: "2020-09-22 13:21",
+          modifyTime: "2020-09-22 13:21",
+          remark: "文字描述",
+        },
+      ],
+      data: [
+        {
+          id: 1,
+          label: "价格区间内组合",
+          children: [
+            {
+              id: 11,
+              label: "0-5元系列",
+              children: [
+                {
+                  id: 111,
+                  label: "0-2元系列",
+                },
+                {
+                  id: 112,
+                  label: "2-5元系列",
+                },
+              ],
+            },
+
+            {
+              id: 12,
+              label: "5-10元系列",
+              children: [
+                {
+                  id: 122,
+                  label: "6-7元系列",
+                },
+                {
+                  id: 123,
+                  label: "7-8元系列",
+                },
+                {
+                  id: 124,
+                  label: "8-10元系列",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 2,
+          label: "按渠道组合",
+          children: [
+            {
+              id: 21,
+              label: "A渠道",
+            },
+             {
+              id: 22,
+              label: "B渠道",
+            },
+          ],
+        },
+        {
+          id: 3,
+          label: "按系列组合",
+          children: [],
+        },
+      ],
     };
   },
   watch: {},
   computed: {},
   methods: {
     /**创建组合 */
-    addAssemble() {},
+    addAssemble() {
+      this.$refs.add.dialogVisible = true;
+    },
     /**搜索 */
-    goSearch(){}
+    goSearch() {},
+    /**点击节点 */
+    handleNodeClick(data) {
+      debugger;
+    },
   },
   mounted() {},
 };
 </script>
 <style lang="scss" scoped>
+.assemble_tree {
+  width: 220px;
+  border-radius: 2px;
+  border: 1px solid #bfcbd9;
+  margin-right: 60px;
+}
+.content {
+  margin-top: 20px;
+}
+</style>
+<style lang="scss">
+.assemble_tree {
+  .el-tree--highlight-current
+    .el-tree-node.is-current
+    > .el-tree-node__content {
+    background-color: #ecf5ff;
+    color: #1d8ce0;
+    // .el-icon-caret-right{
+    //   color: #1d8ce0;
+    // }
+  }
+}
 </style>
---
+++ src/views/assemble/dialogs/addAssemble.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- 新增组合 -->
+  <div>
+    <el-dialog
+      title="创建组合"
+      :visible.sync="dialogVisible"
+      width="420px"
+      :before-close="handleClose"
+      :close-on-click-modal="false"
+      :show-close="false"
+    >
+      <el-form ref="form" :model="form" label-width="70px" style="margin-top:-20px">
+        <el-form-item label="组合名称">
+          <el-input
+            placeholder="请输入组合名称"
+            size="small"
+            v-model="form.name"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="备注说明">
+          <el-input
+            placeholder="请输入备注说明"
+            size="small"
+            v-model="form.tip"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="tip">*集合和产品不能同时存在同一个集合下</div>
+      <span slot="footer" class="dialog-footer">
+        <el-button plain @click="handleClose" size="small">取消</el-button>
+        <el-button @click="confirm" size="small" type="primary"
+          >创建</el-button
+        >
+      </span></el-dialog
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddAssemble",
+  components: {},
+  props: {},
+  data() {
+    return {
+      dialogVisible: false,
+      form: {
+        name: "",
+        tip: "",
+      },
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+      /**取消 */
+    handleClose() {
+      this.dialogVisible = false;
+    },
+    /**创建 */
+    confirm(){}
+  },
+  mounted() {},
+};
+</script>
+<style lang="scss" scoped>
+.tip {
+  color: #fe934f;
+  font-size: 14px;
+}
+</style>
